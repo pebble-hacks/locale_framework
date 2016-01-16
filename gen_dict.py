be7624365e0e83c885193beb79b1a0b0a449a72b
jneubrand <johannes_n@icloud.com>
2016-01-16 12:26:41 -0800

    PEP 8 compliance

--- gen_dict.py
+++ gen_dict.py
@@ -8,6 +8,8 @@ import sys
 
 
 #Helper functions
+
+
 def hash_djb2(string):
     """hash a string using djb2 with seed 5381
     Args:
@@ -27,8 +29,8 @@ def gen_loc_dict(code_dir):
     pbllog_regex = "_\(\"(?P<loc>[^\)]+)\"\)"
 
     for root, dirnames, filenames in os.walk(code_dir):
-        for filename in [filename for filename in filenames \
-                if ".c" in filename[-2:]]:
+        for filename in [filename for filename in filenames
+                         if ".c" in filename[-2:]]:
             fileglob_list.append(os.path.join(root, filename))
     for filename in fileglob_list:
         with open(filename, 'rb') as afile:
@@ -36,9 +38,10 @@ def gen_loc_dict(code_dir):
             match_list = re.finditer(pbllog_regex, text)
             if match_list:
                 for match in match_list:
-                  loc_dict[hash_djb2(match.group('loc'))] = match.group('loc')
+                    loc_dict[hash_djb2(match.group('loc'))] = match.group('loc')
 
     return loc_dict
+
 
 def main():
     # arguments, print an example of correct usage.
@@ -52,18 +55,18 @@ def main():
     code_dir = sys.argv[1]
     output_filename = sys.argv[2]
 
-    hash_dict = gen_loc_dict(code_dir) 
-    json_dict = {str(key) : value for (key, value) in hash_dict.iteritems()}
+    hash_dict = gen_loc_dict(code_dir)
+    json_dict = {str(key): value for (key, value) in hash_dict.iteritems()}
     json.dump(json_dict, open(output_filename, "wb"), indent=2, sort_keys=True)
     print("%s now has %d entries\n" % (output_filename, len(hash_dict)))
     #create binary resource loadable as a pebble dictionary
     with open(output_filename.replace('.json', '.bin'), 'wb') as output_bin:
-      output_bin.write(struct.pack('I', len(hash_dict))) #count of entries
-      for (key, value) in hash_dict.iteritems():
-        output_bin.write(struct.pack('I',key)) #key
-        output_bin.write(struct.pack('I',len(value) + 1)) #length of string including null
-        output_bin.write(value) #write string as c string
-        output_bin.write(struct.pack('B',0)) #null terminate string
+        output_bin.write(struct.pack('I', len(hash_dict)))  # count of entries
+        for (key, value) in hash_dict.iteritems():
+            output_bin.write(struct.pack('I', key))  # key
+            output_bin.write(struct.pack('I', len(value) + 1))  # length of string including null
+            output_bin.write(value)  # write string as c string
+            output_bin.write(struct.pack('B', 0))  # null terminate string
 
 
     #output tuple array
@@ -78,7 +81,5 @@ def main():
     #    output_bin.write(struct.pack('B',0)) #null terminate string
 
 
-
 if __name__ == '__main__':
     main()
-
